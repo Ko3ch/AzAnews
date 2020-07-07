4e0cbfbc887d1d98d23e5da13f3abdf1d6849b07
Ko3ch <tomko3ch@gmail.com>
2020-07-07 19:38:15 +0300

    added configuration

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_SOURCES_API_BASE_URL ='http://newsapi.org/v2/sources?category={}&apiKey={}'
-    NEWS_SOURCE_ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
